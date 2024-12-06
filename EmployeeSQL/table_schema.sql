952749a76d46c8189589f14e4011fd2d01975eb2
Heather <heatherrichardson300@gmail.com>
2024-12-06 13:00:44 -0800

    updates

--- EmployeeSQL/table_schema.sql
+++ EmployeeSQL/table_schema.sql
@@ -1,47 +1,50 @@
 -- Create Departments table
 CREATE TABLE departments (
-	dept_no VARCHAR(5) PRIMARY KEY,
-	dept_name VARCHAR(50) UNIQUE
+	dept_no VARCHAR(5) PRIMARY KEY  NOT NULL,
+	dept_name VARCHAR(50) UNIQUE  NOT NULL
 );
 
 -- Create Titles table
 CREATE TABLE titles (
-	title_id VARCHAR(5) PRIMARY KEY,
-	title VARCHAR(50)
+	title_id VARCHAR(5) PRIMARY KEY NOT NULL,
+	title VARCHAR(50) NOT NULL
 );
 
 -- Create Employees table
 CREATE TABLE employees (
-	emp_no INT PRIMARY KEY,
-	emp_title_id VARCHAR(5),
-	birth_date DATE,
-	first_name VARCHAR(50),
-	last_name VARCHAR(50),
-	gender VARCHAR(1),
-	hire_date DATE,
+	emp_no INT PRIMARY KEY NOT NULL,
+	emp_title_id VARCHAR(5) NOT NULL,
+	birth_date DATE NOT NULL,
+	first_name VARCHAR(50) NOT NULL,
+	last_name VARCHAR(50) NOT NULL,
+	gender VARCHAR(1) NOT NULL,
+	hire_date DATE NOT NULL,
 	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
 );
 
 -- Create Salaries table
 CREATE TABLE salaries (
-	emp_no INT,
-	salary INT,
-	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+	emp_no INT NOT NULL,
+	salary INT NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no), 
+	PRIMARY KEY (emp_no, salary)
 );
 
 -- Create Department Employees table
 CREATE TABLE dept_emp (
-	emp_no INT,
-	dept_no VARCHAR(5),
+	emp_no INT NOT NULL,
+	dept_no VARCHAR(5) NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
-	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	PRIMARY KEY (emp_no, dept_no)
 );
 
 -- Create Department Managers table
 CREATE TABLE dept_manager (
-	dept_no VARCHAR(5),
-	emp_no INT,
+	dept_no VARCHAR(5) NOT NULL,
+	emp_no INT NOT NULL,
 	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
-	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	PRIMARY KEY (dept_no, emp_no)
 );
 
